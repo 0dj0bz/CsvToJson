dc4896fc7d563863544d1eb42f9dabf2e37a6131
0dj0bz <robert.l.abbott@gmail.com>
2014-11-05 23:27:17 -0500

    app.'s with working JSON creation
    Takes in a .CSV file and returns a JSON-formatted result set to the
    browser.

--- node/app.js
+++ node/app.js
@@ -26,18 +26,55 @@ app.post('/', function(req, res){
         fs.readFile ( files.filename.path, function (err, data) {
         	if (err) throw err;
         	console.log (data);
-        	res.type ('text/plain');
-        	res.send (data);
+        	res.type ('text/json');
+
+            var jres = processArray (data);
+            res.send (jres.toString());
+ 
 
         });
-
 
     });	
 
     
-app.use(function(req, res){
-        res.type('text/plain');
-        res.status(303);
-        res.send('File successfully uploaded.');
+    app.use(function(req, res){
+            res.type('text/plain');
+            res.status(303);
+            res.send('File successfully uploaded.');
+    });
 });
-});
+
+function processArray (buffer) 
+{
+    var darry = buffer.toString().split("\r");
+    var outStr = new String();
+
+    outStr = "{ \n\"transactions\" : [\n";
+
+    var colheads = darry[0].split(",");
+
+    for (idx=1;idx<darry.length;idx++) {
+
+        outStr += "  {  \n";
+
+        var arry = darry[idx].split(",");
+
+        for (jdx=0;jdx<colheads.length;jdx++) {
+
+            outStr += "    \"" + colheads[jdx] + "\":\"" +  arry[jdx] + "\"";      
+            if (jdx < colheads.length-1) outStr += ", " 
+            outStr += " \n"  
+        }
+
+        outStr += "  }\n";
+
+
+
+    }
+
+    outStr += "]}";
+
+    return outStr;
+
+
+}
